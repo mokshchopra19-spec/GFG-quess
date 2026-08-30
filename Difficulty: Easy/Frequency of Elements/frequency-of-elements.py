bcbcdb75fa57d2ba90a447ed46cfd699e8998249
mokshchopra19-spec <mokshchopra19@gmail.com>
2026-08-30 16:03:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frequency of Elements/frequency-of-elements.py
+++ Difficulty: Easy/Frequency of Elements/frequency-of-elements.py
@@ -1,6 +1,6 @@
+from collections import Counter
+
 class Solution:
     def countFreq(self, arr):
-       freq={}
-       for x in arr:
-          freq[x] = freq.get(x, 0) + 1
-       return [[key, value] for key, value in freq.items()]
+        freq = Counter(arr)
+        return [[key, value] for key, value in freq.items()]
