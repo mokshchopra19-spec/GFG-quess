8650ca35d2cac1adca9919d317240c0e415c5029
mokshchopra19-spec <mokshchopra19@gmail.com>
2026-09-04 17:30:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Palindrome String/palindrome-string.py
+++ Difficulty: Easy/Palindrome String/palindrome-string.py
@@ -1,4 +1,10 @@
 class Solution:
+    def solve(self,start,end,s):
+        if start>=end:
+            return True
+        if s[start]!=s[end]:
+            return False
+        return self.solve(start+1,end-1,s)    
     def isPalindrome(self, s):
-       return s[::1]==s[::-1]
+        return self.solve(0,len(s)-1,s)
         
